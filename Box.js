953cf1b3e73792c8cb96c96101d253ae217150bc
jaiakashvijayakumar <73417118+jaiakashvijayakumar@users.noreply.github.com>
2020-11-25 10:46:16 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,24 @@
+class Box {
+    constructor(x,y,width,height,angle){
+
+        var options ={
+            restitution: 0.8,
+            
+        }
+
+        this.body = Bodies.rectangle(x,y,width,height,options);
+
+        World.add(world,this.body);
+    }
+
+    display(){
+        
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill(255);
+        rect(pos.x,pos.y,this.width,this.height);
+    
+        
+
+    }
+}
